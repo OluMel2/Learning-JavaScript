b13971c8b19c8576d9bea0ae71e6123a82239038
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-07-24 09:53:29 +0000

    [halfway]2章　JavaScriptの基本文法

---
+++ app2.js
@@ -0,0 +1,35 @@
+//let 変数名 = 内容 ;
+//let hello = 'Hello World';
+//alert(hello);
+
+
+//変数は数値、文字列、関数を代入できる。
+
+// 整数を代入する
+//let int1 = 1;
+// 負数を代入する
+//let int2 = -10;
+// 小数点を代入する
+//let float1 = 3.14;
+// 文字列を代入する
+//let str1 = 'JavaScriptを覚えよう';
+//alert(str1);
+
+
+//四則演算
+// 足し算
+//alert(4 + 3);
+// 引き算
+//alert(8 - 5);
+// 掛け算
+//alert(2 * 6);
+// 割り算
+//alert(10 / 2);
+
+
+//文字列の結合
+//alert('Hello' + 'World');
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
