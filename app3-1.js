d8d182a636b1777e0d179598bfc28b9b5ad86c38
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-08-04 12:22:06 +0000

    [halfway]3章関数じゃんけん

--- app3-1.js
+++ app3-1.js
@@ -13,8 +13,28 @@
 
 //複数の関数を定義する
 
+
 //じゃんけんの手とJavaScriptがランダムに出すじゃんけんの手を比較し、勝ち負けの結果を表示させる関数を定義。
 
-let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+//let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
 
-alert('あなたの選んだ手は' + user_hand + 'です。');
+//alert('あなたの選んだ手は' + user_hand + 'です。');
+
+//このコードを実行しても、入力したじゃんけんと同じものがアラートウィンドウ上に表示されるだけになってしまう。
+
+
+//この処理に、相手のじゃんけんの手をランダムに選択する関数を追加していく。
+
+function getJShand(){
+  let js_hand_num = Math.floor( Math.random() * 3 );
+
+  if(js_hand_num == 0){
+    js_hand = "グー";
+  } else if(js_hand_num == 1){
+    js_hand = "チョキ";
+  } else if(js_hand_num == 2){
+    js_hand = "パー";
+  }
+
+  return js_hand;
+}
